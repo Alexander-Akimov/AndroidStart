04c8bd7a9779e8fd8b9e560cd076b997e4604d88
Lex <akimov.infotech@gmail.com>
2015-12-08 17:24:52 +0600

    Lesson 7: TableLayout, layout_gravity, layout_weight

--- app/src/main/java/com/infotech/p0051_layoutfile/MainActivity.java
+++ app/src/main/java/com/infotech/p0051_layoutfile/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.rlayout);
+        setContentView(R.layout.p007_layout);
+//        setContentView(R.layout.tablelayout);
+//        setContentView(R.layout.rlayout);
 //        setContentView(R.layout.p006_layouts);
 //        setContentView(R.layout.myscreen);
 //        setContentView(R.layout.main_layout);
